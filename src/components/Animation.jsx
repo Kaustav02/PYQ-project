370a50d895d37d1325b5a7b3c7d82e3d9ff194e8
kaustav <kaustav.mondal2003@gmail.com>
2023-05-07 01:06:07 +0530

    first commit

---
+++ src/components/Animation.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import './Animation.css'
+
+const Animation = () => {
+  return (
+    <div>
+
+
+<div className="flower">
+        <div className="f-wrapper">
+            <div className="flower__line"></div>
+            <div className="f">
+                <div className="flower__leaf flower__leaf--1"></div>
+                <div className="flower__leaf flower__leaf--2"></div>
+                <div className="flower__leaf flower__leaf--3"></div>
+                <div className="flower__leaf flower__leaf--4"></div>
+                <div className="flower__leaf flower__leaf--5"></div>
+                <div className="flower__leaf flower__leaf--6"></div>
+                <div className="flower__leaf flower__leaf--7"></div>
+                <div className="flower__leaf flower__leaf--8 flower__fall-down--yellow"></div>
+            </div>
+        </div>
+
+        <div className="f-wrapper f-wrapper--2">
+            <div className="flower__line"></div>
+            <div className="f">
+                <div className="flower__leaf flower__leaf--1"></div>
+                <div className="flower__leaf flower__leaf--2"></div>
+                <div className="flower__leaf flower__leaf--3"></div>
+                <div className="flower__leaf flower__leaf--4"></div>
+                <div className="flower__leaf flower__leaf--5"></div>
+                <div className="flower__leaf flower__leaf--6"></div>
+                <div className="flower__leaf flower__leaf--7"></div>
+                <div className="flower__leaf flower__leaf--8 flower__fall-down--pink"></div>
+            </div>
+        </div>
+
+        <div className="f-wrapper f-wrapper--3">
+            <div className="flower__line"></div>
+            <div className="f">
+                <div className="flower__leaf flower__leaf--1"></div>
+                <div className="flower__leaf flower__leaf--2"></div>
+                <div className="flower__leaf flower__leaf--3"></div>
+                <div className="flower__leaf flower__leaf--4"></div>
+                <div className="flower__leaf flower__leaf--5"></div>
+                <div className="flower__leaf flower__leaf--6"></div>
+                <div className="flower__leaf flower__leaf--7"></div>
+                <div className="flower__leaf flower__leaf--8 flower__fall-down--purple"></div>
+            </div>
+        </div>
+        <div className="flower__glass"></div>
+    </div>
+
+
+    </div>
+
+    
+  )
+}
+
+export default Animation
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Navbar.css'
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    
+
+
+  )
+}
+
+export default Navbar
+
+
+
